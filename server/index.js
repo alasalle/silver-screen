db0a9237253e0a4c8e08f0ef0d7a4a89d6019c80
ANGELINA <angela.lasalle91@gmail.com>
2020-07-07 23:35:37 -0400

    set up server and database

---
+++ server/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const DB_SECRET = process.env.DB_SECRET;
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+mongoose.connect(DB_SECRET, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log("DB CONNECTED"))
+  .catch(err => console.error(err));
+
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Silver Screen!")
+})
+
+app.listen(port, err => {
+  if (err) console.error(err);
+  console.log(`server is listening on port ${port}`);
+});
