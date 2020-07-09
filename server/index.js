e838c7bd8b9d066e91c9aa4351e77fa067855ebd
ANGELINA <angela.lasalle91@gmail.com>
2020-07-08 23:25:58 -0400

    debugged login functionality

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
@@ -10,58 +10,67 @@ const { User } = require("./model/user");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-
-mongoose.connect(config.mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true
-}).then(() => console.log("DB CONNECTED"))
-  .catch(err => console.error(err));
-
+mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("DB CONNECTED"))
+  .catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
-  res.send("Welcome to Silver Screen!")
-})
+  res.send("Welcome to Silver Screen!");
+});
 
 app.post("/api/user/register", (req, res) => {
-
   const user = new User(req.body);
 
   user.save((err, userData) => {
-    if(err) return res.status(400).json({success: false, err})
-  })
-  return res.status(200).json({success: true, user})
-})
+    if (err) return res.status(400).json({ success: false, err });
+    return res.status(200).json({ success: true, userData });
+  });
+});
 
 app.post("/api/user/login", (req, res) => {
   // find the email
 
-  User.findOne({email: req.body.email}, (err, user) => {
-    if (!user) return res.status(400).json({loginSuccess: false, message: "email authentication failed"});
-  })
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res
+        .status(400)
+        .json({ loginSuccess: false, message: "email authentication failed" });
 
-  // compare password with pass hash
+    // compare password with pass hash
 
-  User.comparePassword(req.body.password, (err, isMatch) => {
-    if (!isMatch) {
-      return res.status(400).json({loginSuccess: false, message: "login authentication failed"});
-    }
-  })
-  // generate token
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({
+            loginSuccess: false,
+            message: "login authentication failed",
+          });
+      }
 
-  User.generateToken((err, userData) => {
-    if (err) return res.status(400).send(err);
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
 
-    res.cookie("x_auth", user.token).status(200).json({loginSuccess: true});
-  })
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true });
+      });
+    });
+    // generate token
+  });
+});
 
-})
-
-app.listen(config.port, err => {
+app.listen(config.port, (err) => {
   if (err) console.error(err);
   console.log(`server is listening on port ${config.port}`);
 });
