5115e1cf4802514171a0658c2e261796fd855613
ANGELINA <angela.lasalle91@gmail.com>
2020-07-17 23:28:15 -0400

    added register login component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
+import Register_Login from "./Register_Login";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route path="/about" component={About}/>
+        <Route path="/login" component={Register_Login}/>
       </Switch>
     </div>
   );
