e754e87345f6228ef6f75864f0b2ea34296918ca
ANGELINA <angela.lasalle91@gmail.com>
2020-07-17 22:35:46 -0400

    created store, added provider for redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+
 import "./index.css";
 import App from "./components/App";
+import Reducer from "./reducers"
 import * as serviceWorker from "./serviceWorker";
 
+const createStoreMiddlware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
+
 ReactDOM.render(
+
+  <Provider store={createStoreMiddlware(Reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
   <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
 
