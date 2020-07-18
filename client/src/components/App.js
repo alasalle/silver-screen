a1b594e8d969a4e61b5469563024ada8c692b903
ANGELINA <angela.lasalle91@gmail.com>
2020-07-18 19:32:43 -0400

    not using About page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import About from "./About";
 import Home from "./Home";
 import Register_Login from "./Register_Login";
 
@@ -9,7 +8,6 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={Home}/>
-        <Route path="/about" component={About}/>
         <Route path="/login" component={Register_Login}/>
       </Switch>
     </div>
