d2126adadbd14acae6be91d50d9995ef8961363b
ANGELINA <angela.lasalle91@gmail.com>
2020-07-20 17:25:58 -0400

    rewrote/cleaned up server index

--- server/index.js
+++ server/index.js
@@ -1,62 +1,37 @@
 const express = require("express");
-const app = express();
-const path = require("path");
-const cors = require('cors')
-
 const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
+const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
 
 const config = require("./config/key");
+
+const app = express();
+
+
 const mongoose = require("mongoose");
-
-var jwt = require('express-jwt');
-var jwks = require('jwks-rsa');
-
-const jwtCheck = jwt({
-      secret: jwks.expressJwtSecret({
-          cache: true,
-          rateLimit: true,
-          jwksRequestsPerMinute: 5,
-          jwksUri: 'https://silver-screen.us.auth0.com/.well-known/jwks.json'
-    }),
-    audience: 'https://silver-screen.herokuapp.com/',
-    issuer: 'https://silver-screen.us.auth0.com/',
-    algorithms: ['RS256']
-});
-
-
-
-const connect = mongoose.connect(config.mongoURI,
-  {
-    useNewUrlParser: true, useUnifiedTopology: true,
-    useCreateIndex: true, useFindAndModify: false
-  })
+const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
-app.use(cors())
-// app.use(jwtCheck);
-
-
-
-//to not get any deprecation warning or error
-//support parsing of application/x-www-form-urlencoded post data
-app.use(bodyParser.urlencoded({ extended: true }));
-//to get json data
-// support parsing of application/json type post data
-app.use(bodyParser.json());
-app.use(cookieParser());
-
-app.get('/authorized', function (req, res) {
-  res.send('Secured Resource');
+  const jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://silver-screen.us.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'https://silver-screen.herokuapp.com/',
+  issuer: 'https://silver-screen.us.auth0.com/',
+  algorithms: ['RS256']
 });
 
-app.get('/', (req, res) => {
-  res.json({message: "Welcome to Silver Screen"})
-})
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.use('/api/users', jwtCheck, require('./routes/users'));
-
+app.use('/api/users', require('./routes/users'));
+app.use('/api/comments', require('./routes/comments'));
+app.use('/api/likes', require('./routes/likes'));
+app.use('/api/favorites', require('./routes/favorites'));
 
 //use this to show the image you have in node js server to client (react js)
 //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
@@ -65,20 +40,17 @@ app.use('/uploads', express.static('uploads'));
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
 
-  // Set static folder   
-  // All the javascript and css files will be read and served from this folder
+  // Set static folder
   app.use(express.static("client/build"));
 
-  // index.html for all page routes    html or routing and naviagtion
+  // index.html for all page routes
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
-app.listen(config.port, () => {
-  console.log(`Server Listening on ${config.port}`)
-});
 
-module.exports = {
-  jwtCheck
-}
+
+app.listen(config.port, () => {
+  console.log(`Server Running at ${config.port}`)
+});
