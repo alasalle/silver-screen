8784e8a737c8bae864716ce0ce73b078c92a43ed
ANGELINA <angela.lasalle91@gmail.com>
2020-07-20 21:11:24 -0400

    built landing page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,14 +1,111 @@
-import React from 'react'
-import { FaCode } from "react-icons/fa";
+import React, { useEffect, useState, useRef } from 'react'
+import { Typography, Row, Button } from 'antd';
+import { apiURL, apiKey, imageURL, imageSize, posterSize } from '../../../config/key';
+import MainImage from './Sections/MainImage'
+import GridCard from '../../commons/GridCard'
+const { Title } = Typography;
 
 function LandingPage() {
+    const buttonRef = useRef(null);
+
+    const [Movies, setMovies] = useState([])
+    const [MainMovieImage, setMainMovieImage] = useState(null)
+    const [Loading, setLoading] = useState(true)
+    const [CurrentPage, setCurrentPage] = useState(0)
+
+    useEffect(() => {
+        const endpoint = `${apiURL}/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
+        fetchMovies(endpoint)
+    }, [])
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+    }, [])
+
+
+    const fetchMovies = (endpoint) => {
+
+        fetch(endpoint)
+            .then(result => {
+                console.log({RESULT1: result})
+                return result.json()
+            })
+            .then(result => {
+                console.log({RESULT: result})
+                // console.log(result)
+                // console.log('Movies',...Movies)
+                // console.log('result',...result.results)
+                setMovies([...Movies, ...result.results])
+                setMainMovieImage(MainMovieImage || result.results[0])
+                setCurrentPage(result.page)
+            }, setLoading(false))
+            .catch(error => console.error('Error:', error)
+            )
+    }
+
+    const loadMoreItems = () => {
+        let endpoint = '';
+        setLoading(true)
+        console.log('CurrentPage', CurrentPage)
+        endpoint = `${apiURL}/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${CurrentPage + 1}`;
+        fetchMovies(endpoint);
+
+    }
+
+    const handleScroll = () => {
+        const windowHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
+        const body = document.body;
+        const html = document.documentElement;
+        const docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+        const windowBottom = windowHeight + window.pageYOffset;
+        if (windowBottom >= docHeight - 1) {
+
+            // loadMoreItems()
+            console.log('clicked')
+            buttonRef.current.click();
+
+        }
+    }
+
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+        <div style={{ width: '100%', margin: '0' }}>
+            {MainMovieImage &&
+                <MainImage
+                    image={`${imageURL}${imageSize}${MainMovieImage.backdrop_path}`}
+                    title={MainMovieImage.original_title}
+                    text={MainMovieImage.overview}
+                />
+
+            }
+
+            <div style={{ width: '85%', margin: '1rem auto' }}>
+
+                <Title level={2} > Latest Movies </Title>
+                <hr />
+                <Row gutter={[16, 16]}>
+                    {Movies && Movies.map((movie, index) => (
+                        <React.Fragment key={index}>
+                            <GridCard
+                                image={movie.poster_path ?
+                                    `${imageURL}${posterSize}${movie.poster_path}`
+                                    : null}
+                                movieId={movie.id}
+                                movieName={movie.original_title}
+                            />
+                        </React.Fragment>
+                    ))}
+                </Row>
+
+                {Loading &&
+                    <div>Loading...</div>}
+
+                <br />
+                <div style={{ display: 'flex', justifyContent: 'center', visibility: 'hidden' }}>
+                    <button ref={buttonRef} className="loadMore" onClick={loadMoreItems}>Load More</button>
+                </div>
             </div>
-        </>
+
+        </div>
     )
 }
 
