c0dea917d238293044c09958bf971804ec08a942
ANGELINA <angela.lasalle91@gmail.com>
2020-07-21 00:44:45 -0400

    trying to add a thumbnail to display on link previews

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,11 +5,15 @@ import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer";
 import MovieDetail from "./views/MovieDetail/MovieDetail";
 import FavoritePage from "./views/FavoritePage/FavoritePage";
+import logo from "../assets/logo_transparent.png";
 
 
 function App() {
   return (
     <Suspense fallback={(<div>Loading...</div>)}>
+      <div className="thumbnail">
+        <img src={logo} alt=""/>
+      </div>
       <NavBar />
       <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
         <Switch>
