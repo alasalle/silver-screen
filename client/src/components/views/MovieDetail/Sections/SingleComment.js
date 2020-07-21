8ff81b865fa33c8cd001a0e318f9156d2f9e985f
ANGELINA <angela.lasalle91@gmail.com>
2020-07-20 21:14:24 -0400

    comment component

---
+++ client/src/components/views/MovieDetail/Sections/SingleComment.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react'
+import { Comment, Avatar, Button, Input } from 'antd';
+import axios from 'axios';
+import { useAuth0 } from "@auth0/auth0-react";
+import { useSelector } from 'react-redux';
+import LikeDislikes from './LikeDislikes';
+import { beURL } from "../../../../config/key";
+const { TextArea } = Input;
+
+function SingleComment(props) {
+    const db_user = useSelector(state => state.user);
+    const { user, isAuthenticated, error, isLoading } = useAuth0();
+    const [CommentValue, setCommentValue] = useState("")
+    const [OpenReply, setOpenReply] = useState(false)
+
+    const handleChange = (e) => {
+        setCommentValue(e.currentTarget.value)
+    }
+
+    const openReply = () => {
+        setOpenReply(!OpenReply)
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        const variables = {
+            writer: db_user.userData._id,
+            postId: props.postId,
+            responseTo: props.comment._id,
+            content: CommentValue
+        }
+
+
+        axios.post(`${beURL}/api/comments/saveComment`, variables)
+            .then(response => {
+                if (response.data.success) {
+                    setCommentValue("")
+                    setOpenReply(!OpenReply)
+                    props.refreshFunction(response.data.result)
+                } else {
+                    alert('Failed to save comment')
+                }
+            })
+    }
+
+    const actions = [
+        <LikeDislikes comment commentId={props.comment._id} userId={user.user_metadata.ss_uid} />,
+        <span onClick={openReply} key="comment-basic-reply-to">Reply</span>
+    ]
+
+    return (
+        <div>
+            <Comment
+                actions={actions}
+                author={props.comment.writer.name}
+                avatar={
+                    <Avatar
+                        src={props.comment.writer.image}
+                        alt="image"
+                    />
+                }
+                content={
+                    <p>
+                        {props.comment.content}
+                    </p>
+                }
+            ></Comment>
+
+
+            {OpenReply &&
+                <form style={{ display: 'flex' }} onSubmit={onSubmit}>
+                    <TextArea
+                        style={{ width: '100%', borderRadius: '5px' }}
+                        onChange={handleChange}
+                        value={CommentValue}
+                        placeholder="write some comments"
+                    />
+                    <br />
+                    <Button style={{ width: '20%', height: '52px' }} onClick={onSubmit}>Submit</Button>
+                </form>
+            }
+
+        </div>
+    )
+}
+
+export default SingleComment
