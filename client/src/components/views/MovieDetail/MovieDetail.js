399eca60875bee63564261df9253c5f89e6bb526
ANGELINA <angela.lasalle91@gmail.com>
2020-07-20 21:14:58 -0400

    set up movie detail page

---
+++ client/src/components/views/MovieDetail/MovieDetail.js
@@ -0,0 +1,142 @@
+import React, { useEffect, useState } from 'react'
+import { List, Avatar, Row, Col, Button } from 'antd';
+import axios from 'axios';
+import { useAuth0 } from "@auth0/auth0-react";
+
+import Comments from './Sections/Comments'
+import LikeDislikes from './Sections/LikeDislikes';
+import { apiURL, apiKey, imageURL, imageSize, beURL } from '../../../config/key';
+import GridCards from '../../commons/GridCard';
+import MainImage from '../../views/LandingPage/Sections/MainImage';
+import MovieInfo from './Sections/MovieInfo';
+import Favorite from './Sections/Favorites';
+function MovieDetailPage(props) {
+
+    const movieId = props.match.params.movieId
+    const { user, isAuthenticated, isLoading, error} = useAuth0();
+    const [Movie, setMovie] = useState([])
+    const [Casts, setCasts] = useState([])
+    const [CommentLists, setCommentLists] = useState([])
+    const [LoadingForMovie, setLoadingForMovie] = useState(true)
+    const [LoadingForCasts, setLoadingForCasts] = useState(true)
+    const [ActorToggle, setActorToggle] = useState(false)
+    const movieVariable = {
+        movieId: movieId
+    }
+
+    useEffect(() => {
+
+        let endpointForMovieInfo = `${apiURL}/movie/${movieId}?api_key=${apiKey}&language=en-US`;
+        fetchDetailInfo(endpointForMovieInfo)
+
+        axios.post(`${beURL}/api/comments/getComments`, movieVariable)
+            .then(response => {
+                console.log(response)
+                if (response.data.status) {
+                    console.log('response.data.comments', response.data.comments)
+                    setCommentLists(response.data.comments)
+                } else {
+                    alert('Failed to get comments Info')
+                }
+            })
+
+    }, [])
+
+    const toggleActorView = () => {
+        setActorToggle(!ActorToggle)
+    }
+
+    const fetchDetailInfo = (endpoint) => {
+
+        fetch(endpoint)
+            .then(result => result.json())
+            .then(result => {
+                console.log(result)
+                setMovie(result)
+                setLoadingForMovie(false)
+
+                let endpointForCasts = `${apiURL}/movie/${movieId}/credits?api_key=${apiKey}`;
+                fetch(endpointForCasts)
+                    .then(result => result.json())
+                    .then(result => {
+                        console.log(result)
+                        setCasts(result.cast)
+                    })
+
+                setLoadingForCasts(false)
+            })
+            .catch(error => console.error('Error:', error)
+            )
+    }
+
+    const updateComment = (newComment) => {
+        setCommentLists(CommentLists.concat(newComment))
+    }
+
+    const url = "https://silver-screen.alasalle.com"
+
+    if (isLoading) return <div>Loading...</div>
+
+    return (
+        <div>
+            {/* Header */}
+            {!LoadingForMovie ?
+                <MainImage
+                    image={`${imageURL}${imageSize}${Movie.backdrop_path}`}
+                    title={Movie.original_title}
+                    text={Movie.overview}
+                />
+                :
+                <div>loading...</div>
+            }
+
+
+            {/* Body */}
+            <div style={{ width: '85%', margin: '1rem auto' }}>
+
+                <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                    <Favorite movieInfo={Movie} movieId={movieId} userFrom={user.ss_uid} />
+                </div>
+
+
+                {/* Movie Info */}
+                {!LoadingForMovie ?
+                    <MovieInfo movie={Movie} />
+                    :
+                    <div>loading...</div>
+                }
+
+                <br />
+                {/* Actors Grid*/}
+
+                <div style={{ display: 'flex', justifyContent: 'center', margin: '2rem' }}>
+                    <Button onClick={toggleActorView}>Toggle Actor View </Button>
+                </div>
+
+                {ActorToggle &&
+                    <Row gutter={[16, 16]}>
+                        {
+                            !LoadingForCasts ? Casts.map((cast, index) => (
+                                cast.profile_path &&
+                                <GridCards actor image={cast.profile_path} characterName={cast.characterName} />
+                            )) :
+                                <div>loading...</div>
+                        }
+                    </Row>
+                }
+                <br />
+
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <LikeDislikes video videoId={movieId} userId={user.ss_uid} />
+                </div>
+
+                {/* Comments */}
+                <Comments movieTitle={Movie.original_title} CommentLists={CommentLists} postId={movieId} refreshFunction={updateComment} />
+
+            </div>
+
+        </div>
+    )
+}
+
+export default MovieDetailPage
