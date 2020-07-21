dbb2fa28025dd84e3dd859f05caed0f29facf734
ANGELINA <angela.lasalle91@gmail.com>
2020-07-21 00:18:15 -0400

    trying to fix env var in netlify

--- client/src/config/prod.js
+++ client/src/config/prod.js
@@ -1,9 +1,9 @@
 module.exports = {
-  beURL: process.env.REACT_BE_URL,
-  apiURL: process.env.REACT_API_URL,
-  apiKey: process.env.REACT_API_KEY,
-  imageURL: process.env.REACT_IMAGE_URL,
-  backdropSize: process.env.REACT_BACKDROP_SIZE,
-  imageSize: process.env.REACT_IMG_SIZE,
-  posterSize: process.env.REACT_POSTER_SIZE
+  beURL: process.env.REACT_APP_BE_URL,
+  apiURL: process.env.REACT_APP_API_URL,
+  apiKey: process.env.REACT_APP_API_KEY,
+  imageURL: process.env.REACT_APP_IMAGE_URL,
+  backdropSize: process.env.REACT_APP_BACKDROP_SIZE,
+  imageSize: process.env.REACT_APP_IMG_SIZE,
+  posterSize: process.env.REACT_APP_POSTER_SIZE
 }
