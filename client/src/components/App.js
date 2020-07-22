ba7ecf03e9d6f3c5aec235df26c162cede256a32
ANGELINA <angela.lasalle91@gmail.com>
2020-07-22 03:51:27 -0400

    rid of footer for now

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -22,7 +22,7 @@ function App() {
           <Route exact path="/favorites" component={FavoritePage} />
         </Switch>
       </div>
-      <Footer />
+      {/* <Footer /> */}
     </Suspense>
   );
 }
