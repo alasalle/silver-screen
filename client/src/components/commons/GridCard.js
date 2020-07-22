337cfc02342619eadf3b018df918ae48a496c8e1
ANGELINA <angela.lasalle91@gmail.com>
2020-07-22 03:40:34 -0400

    fixed actor grid cards

--- client/src/components/commons/GridCard.js
+++ client/src/components/commons/GridCard.js
@@ -1,32 +1,51 @@
-import React from 'react'
-import { Card, Avatar, Col, Typography, Row } from 'antd';
-import { imageURL } from '../../config/key';
+import React from "react";
+import { Card, Avatar, Col, Typography, Row } from "antd";
+import { imageURL } from "../../config/key";
+
+const { Meta } = Card;
 
 function GridCards(props) {
+  let { actor, keyProp, image, movieId, movieName, characterName, name } = props;
+  const POSTER_SIZE = "w154";
 
-    let { actor, key, image, movieId, movieName, characterName } = props
-    const POSTER_SIZE = "w154";
-
-    if (actor) {
-        return (
-            <Col key={key} lg={6} md={8} xs={24}>
-                <div style={{ position: 'relative' }}>
-                    <img style={{ width: '100%', height: '320px' }} alt={characterName} src={`${imageURL}${POSTER_SIZE}${image}`} />
-                </div>
-            </Col>
-        )
-    } else {
-        return (
-            <Col key={key} lg={6} md={8} xs={24}>
-                <div style={{ position: 'relative' }}>
-                    <a href={`/movie/${movieId}`} >
-                        <img style={{ width: '100%', height: '320px' }} alt={movieName} src={image} />
-                    </a>
-                </div>
-            </Col>
-        )
-    }
-
+  if (actor) {
+    return (
+      <Col key={keyProp} lg={6} md={8} xs={24}>
+        <div style={{ position: "relative" }}>
+          <Card
+            hoverable
+            style={{ width: "100%"}}
+            cover={
+              <img
+                style={{ width: "100%" }}
+                alt={characterName}
+                src={`${imageURL}${POSTER_SIZE}${image}`}
+              />
+            }
+          >
+            <Meta
+              title={name}
+              description={`Character: ${characterName}`}
+            />
+          </Card>
+        </div>
+      </Col>
+    );
+  } else {
+    return (
+      <Col key={keyProp} lg={6} md={8} xs={24}>
+        <div style={{ position: "relative" }}>
+          <a href={`/movie/${movieId}`}>
+            <img
+              style={{ width: "100%", height: "320px" }}
+              alt={movieName}
+              src={image}
+            />
+          </a>
+        </div>
+      </Col>
+    );
+  }
 }
 
-export default GridCards
+export default GridCards;
