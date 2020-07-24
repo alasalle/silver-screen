c6247e9d8bc19698b4a759da30590e7e273e3efc
ANGELINA <angela.lasalle91@gmail.com>
2020-07-23 21:08:06 -0400

    redirect Faves page to login when not authed

--- client/src/components/views/NavBar/Sections/LeftMenu.js
+++ client/src/components/views/NavBar/Sections/LeftMenu.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Menu } from "antd";
-
+import { useAuth0 } from "@auth0/auth0-react";
 function LeftMenu(props) {
+  const { isLoading, isAuthenticated } = useAuth0();
   return (
     <Menu mode={props.mode} className="leftMenu">
       <Menu.Item key="home">
         <a href="/">Home</a>
       </Menu.Item>
-      <Menu.Item key="faves">
-        <a href="/favorites">Favorites</a>
-      </Menu.Item>
+      {!isLoading && isAuthenticated && (
+        <Menu.Item key="faves">
+          <a href="/favorites">Favorites</a>
+        </Menu.Item>
+      )}
     </Menu>
   );
 }
